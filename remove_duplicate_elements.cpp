8f74a7a41efc5227000ea7cb377a7d0b264d2f58
KhushiRawat310 <96656792+KhushiRawat310@users.noreply.github.com>
2024-07-15 19:40:30 +0530

    remove_duplicate_elements.cpp

--- remove_duplicate_elements.cpp
+++ remove_duplicate_elements.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        int i=0,j=0,k;
+        {
+            while(i<n)
+            {
+                if(nums[i]==nums[j])
+                {
+                    i++;
+                }
+                else
+                {
+                    j++;
+                    nums[j]=nums[i];
+                }
+               k=j+1;  
+            }
+        }
+        return k;
+    }
+};
